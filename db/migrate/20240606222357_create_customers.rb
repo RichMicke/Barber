a08b2f15adf2c572b035d48c2187389c9bbb790c
Eljuan25 <147083751+Eljuan25@users.noreply.github.com>
2024-06-18 15:03:17 -0600

    This is the change of models, and I also corrected some small changes from custmers to custemer because it marked an error (#1)

--- db/migrate/20240606222357_create_customers.rb
+++ db/migrate/20240606222357_create_customers.rb
@@ -1,6 +1,6 @@
 class CreateCustomers < ActiveRecord::Migration[7.1]
   def change
-    create_table :customer do |t|
+    create_table :customers do |t|
       t.string :name 
       t.string :phone
       t.string :email
